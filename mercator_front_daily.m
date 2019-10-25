0ac3e54c2d395375fce443b34bf5e1961f9c4425
cdmpbp123 <359304296@qq.com>
2019-10-25 10:04:51 +0200

    test change to new structure

--- mercator_front_daily.m
+++ mercator_front_daily.m
@@ -3,7 +3,7 @@ close all
 clear all
 clc
 %
-platform = 'mercator_PC';
+platform = 'hanyh_laptop';
 
 if strcmp(platform, 'hanyh_laptop')
     basedir = 'D:\lomf\frontal_detect\';
@@ -63,7 +63,8 @@ sigma = 2;
 N = 2;
 fill_value = 0;
 % detect parameter
-flen_crit = 0;
+% flen_crit = 0;
+flen_crit = []; % auto-thresh for length
 thresh_in = [];
 % postprocess parameter
 logic_morph = 0;
@@ -73,13 +74,14 @@ yy1 = 2018;
 yy2 = 2018;
 
 for iy = yy1:yy2
-    year_result_path = [result_path, num2str(iy), '/']; mkdir(year_result_path)
+    year_mat_path = [result_path, num2str(iy), '/mat/']; mkdir(year_mat_path)
+    year_netcdf_path = [result_path, num2str(iy), '/netcdf/']; mkdir(year_netcdf_path)
     year_fig_path = [fig_path, num2str(iy), '/']; mkdir(year_fig_path)
 
     for im = 1:12
 
         for id = 1:31
-            day_string = [num2str(iy), num2str(im, '%2.2d'), num2str(id, '%2.2d')];
+            day_string = [num2str(iy), num2str(im, '%2.2d'), num2str(id, '%2.2d')]
             filestruct = dir([data_path, '/ext-PSY4V3R1_1dAV_', day_string, '*gridT*.nc']);
 
             if isempty(filestruct)
@@ -89,21 +91,30 @@ for iy = yy1:yy2
             fn = [data_path, filestruct.name];
             [temp, grd] = mercator_preprocess(fn, depth, lon_w, lon_e, lat_s, lat_n, skip);
             [temp_zl] = variable_preprocess(temp, smooth_type, fill_value);
-            [tfrontline, bw_final, thresh_out, tgrad, tangle] = front_line(temp_zl, thresh_in, grd, flen_crit, logic_morph);
+            [tfrontline, bw_line, thresh_out, tgrad, tangle] = front_line(temp_zl, thresh_in, grd, flen_crit, logic_morph);
             fnum = length(tfrontline);
-            [info_area, tfrontarea] = front_area(tfrontline, tgrad, tangle, grd, thresh_out);
+            [tfrontarea,bw_area] = front_area(tfrontline, tgrad, tangle, grd, thresh_out);
+            [front_parameter] = cal_front_parameter(tfrontline,tfrontarea,grd);
             % dump output
-            dump_type = 'MAT';
-            result_fn = [year_result_path, 'detected_front_', day_string, '.mat'];
-            dump_front_stats(dump_type, result_fn, bw_final, temp_zl, ...
-                grd, tfrontline, tfrontarea, info_area, ...
-                thresh_out, skip, flen_crit, datatype, smooth_type, fntype)
+            dump_front_stats('MAT', [year_mat_path, 'detected_front_', day_string, '.mat'], ...
+                bw_line, bw_area, temp_zl, ...
+                grd, tfrontline, tfrontarea, front_parameter,...
+                thresh_out, skip, flen_crit, ...
+                datatype, smooth_type, fntype)
+            % NetCDF output
+            dump_front_stats('netcdf', [year_netcdf_path,'detected_front_',day_string,'.nc'], ...
+                bw_line, bw_area, temp_zl, ...
+                grd, tfrontline, tfrontarea, front_parameter,...
+                thresh_out, skip, flen_crit, ...
+                datatype, smooth_type, fntype)
             % plot figures
             fig_type = 'front_product';
             fig_show = 'off';
             fig_fn = [year_fig_path, 'mercator_front_product_', day_string, '.png'];
-            plot_front_figure(lon_w, lon_e, lat_s, lat_n, fig_fn, bw_final, temp_zl, ...
-                grd, tfrontline, tfrontarea, info_area, datatype, fig_type, fig_show);
+            plot_front_figure(lon_w, lon_e, lat_s, lat_n, fig_fn, ...
+                bw_line, bw_area, temp_zl, ...
+                grd, tfrontline, tfrontarea, front_parameter, ...
+                datatype, fig_type, fig_show);
             close all
 
         end
