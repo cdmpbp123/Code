790341c0e356fd4063e5577d970a77a6276befaa
cdmpbp123 <359304296@qq.com>
2019-11-06 22:47:54 +0100

    minor change of function concatenate multiple year variable

--- concatenate_front_multipe_years.m
+++ concatenate_front_multipe_years.m
@@ -1,10 +1,13 @@
-function concat_var = concatenate_front_multipe_years(daily_path, varname,yy1,yy2)
+function concat_var = concatenate_front_multipe_years(daily_path, varname,dimsize,yy1,yy2)
 % function only for this script
 concat_var = [];
 for yy = yy1:yy2
     fn = [daily_path, '/concatenate_front_daily_',num2str(yy),'.nc'];
+    if ~exist(fn)
+        continue
+    end
     tmp = ncread(fn,varname);
-    concat_var = cat(3,concat_var,tmp);
+    concat_var = cat(dimsize,concat_var,tmp);
 end
 
 end
